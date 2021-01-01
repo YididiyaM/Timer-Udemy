eaefa01ae3d11403c470811dc7981f8d5509cd0d
Doua Halwa <duaah1980@gmail.com>
2021-01-01 20:39:47 +0100

    ticker in the console, starts and pauses

--- index.js
+++ index.js
@@ -5,10 +5,20 @@ class Timer {
         this.pauseButton = pauseButton;
 
         this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
     }
 
-    start(){
-        console.log('Time to start')
+    start = () =>{
+        this.tick();
+        this.intervalId = setInterval(this.tick, 1000)
+    }
+
+    pause = () =>{
+        clearInterval(this.intervalId);
+    };
+
+    tick = () =>{
+        console.log('tick');
     }
 }
 
@@ -16,4 +26,6 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer (durationInput, startButton, pauseButton)
+const timer = new Timer (durationInput, startButton, pauseButton);
+
+timer.start();
